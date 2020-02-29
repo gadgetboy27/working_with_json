b562f2bee91241a99ab21fbe52f6004da346fcef
Henry-Peti <iamgadgetboy@gmail.com>
2020-02-29 21:15:57 +1300

    full states search function with json file call

--- main.js
+++ main.js
@@ -1,15 +1,61 @@
-fetch('draws.json')
-            .then((resp) => resp.json())
-            .then((json) => {
-                // console.log(json)
-            let draw = json.draw
-            let output = ''
-            // for(let i = 0; i < draw.length; i++)
-            draw.forEach(function(draw){
-                output += "<li>" + draw + "</li>"
-            })
-            document.getElementById('draw').innerHTML = output
-        
+const search = document.getElementById('search')
+const matchList = document.getElementById('match-list')
+
+// Search numbers used in DB
+ const searchNumber = async searchText => {
+    const res = await fetch('draws.json')
+    const data = await res.json()
+
+    // console.log(data)
+    // Get text match
+    let matches = data.filter(call => {
+        const regex = new RegExp(`^${searchText}`, 'gi')
+        return call.name.match(regex) || state.abbr.match(regex)
+    })
     
-        })
+    if (searchText.length === 0) {
+        matches = []
+        matchList.innerHTML = ''
+    }
+
+    outputHtml(matches)
+ }
+// Show in HTML
+ const outputHtml = matches => {
+     if(matches.length > 0) {
+         const html = matches.map(match => `
+         <div class="card card-body mb-1">
+         <h4>${match.name} (${match.abbr}) <span class="text-primary">
+         ${match.capital}</span></h4>
+         <small>Lat: ${match.lat} / Long: ${match.long}</small>
+         </div>
+         `)
+         .join()
+
+        matchList.innerHTML = html
+     }
+ }
+
+  search.addEventListener('input', () => searchNumber(search.value))
+
+
+
+//     if (searchText.length === 0) {
+//         matches = []
+//     }
+//     console.log(numbers)
+    
+// }
+
+// const outputHtml = matches => {
+//     if(matches.length > 0) {
+//         const html = matches.map(match => `
+//         <div class="card card-body mb-1">
+//             <h4>${match.draw}</h4>`
+//         )
+//     }
+// }
+
+
+
     
